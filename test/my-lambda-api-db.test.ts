bfc65e6cf9766f4dad8af5505dd927e2fd6ceecb
Joey Zhao <5253430+joeyzhao2018@users.noreply.github.com>
2025-04-03 11:27:47 -0400

    Initial commit

---
+++ test/my-lambda-api-db.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyLambdaApiDb from '../lib/my-lambda-api-db-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/my-lambda-api-db-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyLambdaApiDb.MyLambdaApiDbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
